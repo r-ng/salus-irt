9f3944bcd811a146904dddbdc9433de93810264c
Yves Pares <yves.pares@gmail.com>
2012-10-11 19:50:00 +0200

    Fields options parsing enhanced

--- src/salus/irt/views/generation/icsr_form.clj
+++ src/salus/irt/views/generation/icsr_form.clj
@@ -9,6 +9,16 @@
           (list? n)   ::field-with-opts
           (string? n) ::simple-field))
 
+(defn query-map [m & clauses]
+    (if clauses
+        (let [[keys func & clauses-rest] clauses]
+            (if (and keys func)
+                (let [values (map m keys)]
+                    (if (reduce #(and %1 %2) true keys)
+                        (apply func values)
+                        (query-map m clauses-rest)))
+                (throw (IllegalArgumentException. "query-map requires a even number of forms"))))))
+
 
 (defmulti icsr-node->hiccup (fn [node & args] (node-type node)))
 ; Returns a _list_ of hiccup nodes
@@ -22,8 +32,9 @@
 
 (defmethod icsr-node->hiccup ::field-with-opts [[field-name & {:as opts}] prefix suffix]
     (let [full-name (str prefix field-name suffix)
-          field     (if-let [o (:select-in opts)] (h/drop-down full-name o)
-                                                  (h/text-field full-name))]
+          field     (query-map opts
+                        [:select-in]   #(h/drop-down full-name %)
+                        []             #(h/text-field full-name))]
         (if-let [l (:label opts)]
             (if (= l :none) field
                             (list (h/label l l) field))
